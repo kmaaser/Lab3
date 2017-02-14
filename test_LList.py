94d820e69962a9923c2f2c21439ec42482e1dc88
kmaaser <kmaaser@capital.edu>
2017-02-13 22:14:20 -0500

    Working on test_LList

--- test_LList.py
+++ test_LList.py
@@ -3,3 +3,29 @@
 # Karenna Maaser
 # 02/13/2012
 #----------------------------------------------------------------------
+
+import sys
+import unittest
+
+sys.path.insert(0, '..')
+from LList import *
+
+class testLList(unittest.TestCase):
+
+    #----------------------------------------------------------------------
+
+    def testLen(self):
+        a = LList()
+        a.append(1)
+        a.append(2)
+        a.append(3)
+
+        self.assertEqual(a.LList, 3)
+
+    #----------------------------------------------------------------------
+
+def main(argv):
+    unittest.main()
+
+if __name__ == '__name__':
+    main(sys.argv)
