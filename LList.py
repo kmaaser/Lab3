2e6657d8f57f3059c20d3b1f93bc586dd4b3f443
kmaaser <kmaaser@capital.edu>
2017-02-13 21:58:30 -0500

    Changed name of file and started test file

--- LList.py
+++ LList.py
@@ -1,14 +1,14 @@
 #----------------------------------------------------------------------
-# ListNode.py
+# LList.py
 # Karenna Maaser
-# 01/28/2012
+# 02/13/2012
 #----------------------------------------------------------------------
 
 import sys
 
 #----------------------------------------------------------------------
 
-class ListNode:
+class LList:
 
     '''A single-linked structure'''
     
@@ -108,6 +108,19 @@ class ListNode:
 
             # change self.head to point "over" the deleted node
             self.head = self.head.link
+        elif position == (self.size - 1):
+            # find the node immediately before the one to delete
+            pNode = self._find(position - 1)
+
+             # save the item from the node to delete
+            item = pNode.link.item
+
+            # changes the tail to the predecessor
+            pNode.link = None
+            pNode = self.tail
+        elif self.size == 1:
+            self.head = None
+            self.tail = None
         else:
             # find the node immediately before the one to delete
             prev_node = self._find(position -1)
@@ -127,14 +140,14 @@ class ListNode:
         post: x is appended onto the end of the list'''
 
         # create a new node containing x
-        newNode = ListNode(x)
+        newNode = LList(x)
 
         # link it into the end of the list
         if self.head is not None:
             # non-empty list
-            node = self._find(self.size - 1)
-            node.link = newNode
-            self.tail = newNode
+            self.tail.link = newNode
+            newNode.link = None
+            self.tail.link = newNode
         else:
             # empty list
             # set self.head to new node
@@ -154,11 +167,16 @@ class ListNode:
 
         if i == 0:
             # insert before position 0 requires updating self.head
-            self.head = ListNode(x, self.head)
+            self.head = LList(x, self.head)
+        elif i == (self.size - 1):
+            newNode = LList(x)
+            self.tail.link = newNode.item
+            newNode.link = None
+            newNode = self.tail
         else:
             # find item that node is to be inserted after
             prev = self._find(i -1)
-            prev.link = ListNode(x, prev.link)
+            prev.link = LList(x, prev.link)
         self.size += 1
 
 #----------------------------------------------------------------------
@@ -175,7 +193,10 @@ class ListNode:
         # default is to delete the last item
         # i could be zero so need to compare to None
         if i is None:
+            self.tail = self._find(i -1)
             i = self.size - 1
+            self._delete(i)
+        self.size -= 1
 
         return self._delete(i)
 
@@ -188,7 +209,13 @@ class ListNode:
         post: if x is in the list from position start to the end, the position
         it is located at is returned, otherwise a ValueError is raised'''
 
-        pass
+        start = 0
+        for i in LList:
+            start += 1
+            if i == x:
+                yield start
+            else:
+                return ValueError
 
 #----------------------------------------------------------------------
     def remove(self, x):
@@ -196,7 +223,11 @@ class ListNode:
         pre: None
         post: if x is in the list, the first instance of x is removed from
         the list, otherwise a ValueError is raised'''
-
+        for i in LList:
+            if i == x:
+                self._delete(x)
+            else:
+                return ValueError
         pass
 
 #----------------------------------------------------------------------
@@ -205,16 +236,20 @@ class ListNode:
         pre: none
         post: each item in the list l is appended onto the list'''
 
-        pass
+        l = LList()
+        for i in l:
+            LList.append(i)
+            self.size += 1
 
 #----------------------------------------------------------------------
     def __copy__(self):
         '''returns a new LList that contains the same items as self'''
 
-        a = ListNode()
+        a = LList()
         for i in range(len(self)):
             a.append(self[i])
             if self[i] == 0:
                 self.head = i
+                self.tail = None
 
 #----------------------------------------------------------------------
